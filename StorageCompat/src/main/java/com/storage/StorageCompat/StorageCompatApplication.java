de9e4c119dc9b04de4bb022a6c37ce8d35a16fd6
gian <clabitomello@gmail.com>
2024-05-10 02:23:56 -0300

    Create project

---
+++ StorageCompat/src/main/java/com/storage/StorageCompat/StorageCompatApplication.java
@@ -0,0 +1,13 @@
+package com.storage.StorageCompat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageCompatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageCompatApplication.class, args);
+	}
+
+}
---
+++ StorageCompat/src/test/java/com/storage/StorageCompat/StorageCompatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storage.StorageCompat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageCompatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
